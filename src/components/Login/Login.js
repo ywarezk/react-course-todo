eebf484daab04a85d432921ff7e748a7f4bbb45c
orit <oprince10@gmail.com>
2021-12-19 15:57:07 +0200

    added solution for forms

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,12 @@ import { useState } from "react";
 export function Login() {
     const [user, setUser] = useState(null)
 
+    // state var to contain form values
+    const [ formValues, setFormValues ] = useState({
+        email: '',
+        password: ''
+    })
+
     const handleSubmit = async ( e ) => {
         // 
         console.log('this will run when submitting the form');
@@ -14,8 +20,8 @@ export function Login() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                email: 'yariv@nerdeez.com',
-                password: '12345678'
+                email: formValues.email,
+                password: formValues.password
             })
         })
         const userFromServer = await response.json();
@@ -48,16 +54,73 @@ export function Login() {
         */
     }
 
+    /**
+     * update the state with the new email
+     * @param {*} e 
+     */
+    /*
+    const handleEmailChange = ( e ) => {
+        // {email: '', password: ''}
+        setFormValues({
+            // email and password from the old object
+            ...formValues,
+            // override the email with the new value
+            email: e.target.value
+        })
+    }
+
+    const handlePasswordChange = ( e ) => {
+        // {email: '', password: ''}
+        setFormValues({
+            ...formValues,
+            password: e.target.value
+        })
+    }
+    */
+
+    /**
+     * @params key email/password
+     * 
+     */
+    /*
+    handleChange = (key, value) => {        
+        // formValues[key] = value
+        setFormValues({
+            ...formValues,
+            [key]: value
+            // ...(key === 'email' ? {email: value} : {password: value})
+            // key === 'email' ? 
+        })
+    }
+    */
+
+    const handleChange = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <form onSubmit={ handleSubmit }>
             <input 
+                value={formValues.email}
+                onChange={ handleChange }
+                
+                
+                // onChange={ (e) => setFormValues({
+                //     ...formValues,
+                //     email: e.target.value
+                // }) }
                 type="email"
                 name="email"
                 placeholder="Enter your email"
             />
             <input 
+                value={formValues.password}
                 type="password"
                 name="password"
+                onChange={ handleChange }
                 placeholder="Enter your password"
             />
 
